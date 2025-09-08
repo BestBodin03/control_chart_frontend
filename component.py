14eb504f1219ff202b6c6a752e2df96a9554a3e7
BestBodin03 <bodinsaensuk@gmail.com>
2025-09-08 17:14:35 +0700

    Adjust some code

--- component.py
+++ component.py
@@ -1,484 +1,90 @@
-import 'dart:math';
+import 'package:dio/dio.dart';
 
-import 'package:control_chart/data/bloc/search_chart_details/search_bloc.dart';
-import 'package:control_chart/domain/models/chart_data_point.dart';
-import 'package:control_chart/domain/models/control_chart_stats.dart';
-import 'package:control_chart/ui/core/design_system/app_color.dart';
-import 'package:control_chart/ui/core/design_system/app_typography.dart';
-import 'package:control_chart/ui/core/shared/dashed_line_painter.dart' show DashedLinePainter;
-import 'package:control_chart/ui/core/shared/table_component.dart';
-import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/material.dart';
+class ApiConfig {
+  final Dio _dio = Dio(BaseOptions(baseUrl: ""));
 
-  
-class ControlChartComponent extends StatelessWidget {
-  final List<ChartDataPoint>? dataPoints;
-  final ControlChartStats? controlChartStats;
-  final String xAxisLabel;
-  final String yAxisLabel;
-  final Color? dataLineColor;
-  final Color? backgroundColor;
-  final double? height;
-  final double? width;
+  Future<T> get<T>(String endpoint) async {
+    final response = await _dio.get(endpoint);
+      return response as T;
+  }
 
-  const ControlChartComponent({
-    super.key,
-    this.dataPoints,
-    this.controlChartStats,
-    this.xAxisLabel = 'Date (mm/dd)',
-    this.yAxisLabel = 'Surface Hardness',
-    this.dataLineColor = AppColors.colorBrand,
-    this.backgroundColor,
-    this.height = 240,
-    this.width = 560,
-  });
-  
-  @override
-  Widget build(BuildContext context) {
-    if (dataPoints == null || dataPoints!.isEmpty) {
-      return const Center(child: Text('No data available'));
+  Future<T> getQueryParam<T>(
+    String endpoint, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+  }) async {
+    try {
+      final response = await _dio.get(
+        endpoint,
+        queryParameters: queryParameters,
+        options: options,
+      );
+      return response.data as T;
+    } catch (e) {
+      throw Exception('GET request failed: $e');
     }
+  }
 
-    return Container(
-      height: height,
-      width: width,
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: backgroundColor ?? Colors.white,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        children: [
-          // Chart Title
-          // Text(
-          //   'Control Chart',
-          //   style: AppTypography.textBody3B,
-          // ),
-          const SizedBox(height: 16),
-          
-          // The LineChart
-          Expanded(
-            child: LineChart(
-              LineChartData(
-                gridData: buildGridData(),
-                titlesData: buildTitlesData(),
-                borderData: buildBorderData(),
-                lineBarsData: buildLineBarsData(),
-                extraLinesData: buildControlLines(),
-                lineTouchData: buildTouchData(),
-                minX: 0,
-                maxX: (dataPoints!.length - 1).toDouble(),
-                minY: getMinY(),
-                maxY: getMaxY(),
-              ),
-            ),
-          ),
-          
-          const SizedBox(height: 16),
-          
-          // Axis Labels
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              // Y-axis label (rotated)
-              RotatedBox(
-                quarterTurns: 3,
-                child: Text(
-                  yAxisLabel, // Surface Hardness
-                  style: const TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black87,
-                  ),
-                ),
-              ),
-              // X-axis label
-              Text(
-                xAxisLabel, // Date (mm/dd)
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black87,
-                ),
-              ),
-            ],
-          ),
-          
-          const SizedBox(height: 16),
-          
-          // Legend
-          buildLegend(),
-        ],
+  Future<T> post<T>(String endpoint, dynamic data) async {
+    try {
+      final res = await _dio.post(
+        endpoint,
+        data: data,
+        options: Options(
+          headers: {'Content-Type': 'application/json'},
+          validateStatus: (_) => true,
+        ),
+      );
+
+      print('POST $endpoint -> ${res.statusCode}\n${res.data}');
+
+      if (res.statusCode != null && res.statusCode! >= 200 && res.statusCode! < 300) {
+        return res.data as T; // <-- คืน JSON (data) ไม่ใช่ Response
+      }
+
+      throw DioException(
+        requestOptions: res.requestOptions,
+        response: res,
+        type: DioExceptionType.badResponse,
+        error: 'HTTP ${res.statusCode}: ${res.data}',
+      );
+    } on DioException catch (e) {
+      print('DioException: ${e.response?.statusCode} ${e.response?.data}');
+      rethrow;
+    }
+  }
+
+  Future<T> patch<T>(String endpoint, {dynamic data}) async {
+    final res = await _dio.patch(
+      endpoint,
+      data: data,
+      options: Options(
+        headers: {'Content-Type': 'application/json'},
+        validateStatus: (_) => true,
       ),
     );
-  }
-  
-  FlGridData buildGridData() {
-    return FlGridData(
-      show: true,
-      drawHorizontalLine: true,
-      drawVerticalLine: true,
-      horizontalInterval: _calculateYAxisInterval(),
-      // horizontalInterval: 24,
-      verticalInterval: 24,
-      getDrawingHorizontalLine: (value) {
-        return FlLine(
-          color: Colors.grey.shade300,
-          strokeWidth: 0.5,
-        );
-      },
-      getDrawingVerticalLine: (value) {
-        return FlLine(
-          color: Colors.grey.shade300,
-          strokeWidth: 0.5,
-        );
-      },
-    );
-  }
 
-  FlTitlesData buildTitlesData() {
-    return FlTitlesData(
-        leftTitles: AxisTitles(
-        axisNameSize: 36, // กำหนดขนาด axis name
-        axisNameWidget: SizedBox(
-          width: height,
-          child: Text(
-            yAxisLabel,
-            style: AppTypography.textBody3BBold,
-            textAlign: TextAlign.center,
-          ),
-        ),
-        sideTitles: SideTitles(
-          showTitles: true,
-          reservedSize: 40,
-          interval: _calculateYAxisInterval(),
-          getTitlesWidget: (value, meta) {
-            return Text(
-              value.toStringAsFixed(1),
-              style: const TextStyle(
-                color: Colors.black54,
-                fontSize: 12,
-              ),
-            );
-          },
-        ),
-        ),
-      bottomTitles: AxisTitles(
-        axisNameSize: 36, // กำหนดขนาด axis name
-        axisNameWidget: SizedBox(
-          width: width,
-          child: Text(
-            xAxisLabel,
-            style: AppTypography.textBody3BBold,
-            textAlign: TextAlign.center,
-          ),
-        ),
-        sideTitles: SideTitles(
-          showTitles: true,
-          reservedSize: 32, // เพิ่มขนาดสำหรับ X-axis labels
-          interval: _calculateXInterval(),
-          getTitlesWidget: (value, meta) {
-            final index = value.toInt();
-            if (index >= 0 && index < dataPoints!.length) {
-              return Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Text(
-                  dataPoints![index].label,
-                  style: const TextStyle(
-                    color: Colors.black54,
-                    fontSize: 10, // เพิ่มขนาดฟอนต์
-                  ),
-                ),
-              );
-            }
-            return const SizedBox.shrink();
-          },
-        ),
-      ),
-      topTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
-      rightTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
-    );
-  }
+    print('PATCH $endpoint -> ${res.statusCode}\n${res.data}');
 
-  FlBorderData buildBorderData() {
-    return FlBorderData(
-      show: true,
-      border: Border.all(
-        color: Colors.black54,
-        width: 1,
-      ),
-    );
-  }
-
-  ExtraLinesData buildControlLines() {
-    return ExtraLinesData(
-      extraLinesOnTop: false,
-      horizontalLines: [
-        // USL (Upper Specification Limit)
-        if ((controlChartStats?.specAttribute?.surfaceHardnessUpperSpec ?? 0.0) > 0.0)
-          HorizontalLine(
-            y: controlChartStats!.specAttribute!.surfaceHardnessUpperSpec!,
-            color: Colors.red.shade400,
-            strokeWidth: 2,
-          ),
-        
-        HorizontalLine(
-          y: controlChartStats?.controlLimitIChart?.ucl ?? 0.0,
-          color: Colors.amberAccent,
-          strokeWidth: 1.5,
-        ),
-        
-        // Average Line
-        HorizontalLine(
-          y: controlChartStats?.average ?? 0.0,
-          color: AppColors.colorSuccess1,
-          strokeWidth: 2,
-        ),
-
-        HorizontalLine(
-          y: controlChartStats?.controlLimitIChart?.lcl ?? 0.0,
-          color: Colors.amberAccent,
-          strokeWidth: 1.5,
-        ),
-        
-        // LSL (Lower Specification Limit)
-      if ((controlChartStats?.specAttribute?.surfaceHardnessLowerSpec ?? 0.0) > 0.0)
-        HorizontalLine(
-          y: controlChartStats!.specAttribute!.surfaceHardnessLowerSpec!,
-          color: Colors.red.shade400,
-          strokeWidth: 2,
-        ),
-      ],
-    );
-  }
-
-  List<LineChartBarData> buildLineBarsData() {
-    final interval = _calculateXInterval().toInt();
-    
-    return [
-      LineChartBarData(
-        spots: dataPoints!
-            .asMap()
-            .entries
-            .where((entry) => entry.key % interval == 0)
-            .map((entry) => FlSpot(entry.key.toDouble(), entry.value.value))
-            .toList(),
-        
-        isCurved: false,
-        color: dataLineColor,
-        barWidth: 4,
-        isStrokeCapRound: true,
-        dotData: FlDotData(
-          show: true,
-          getDotPainter: (spot, percent, barData, index) {
-            final realIndex = spot.x.toInt();
-            final value = dataPoints![realIndex].value;
-            Color dotColor = dataLineColor!;
-            
-            // OVER LIMIT #RULE 1
-            if ((controlChartStats?.specAttribute?.surfaceHardnessUpperSpec ?? 0.0) > 0.0 &&
-              (value > (controlChartStats?.specAttribute?.surfaceHardnessUpperSpec ?? 0.0) || 
-                value < (controlChartStats?.specAttribute?.surfaceHardnessLowerSpec ?? 0.0))) {
-            dotColor = Colors.red; // Out of spec
-            } else if (value > (controlChartStats?.controlLimitIChart?.ucl ?? 0.0) || 
-                      value < (controlChartStats?.controlLimitIChart?.lcl ?? 0.0)) {
-            dotColor = Colors.orange; // Warning zone
-            }
-            
-            return FlDotCirclePainter(
-              radius: 4,
-              color: dotColor,
-              strokeWidth: 2,
-              strokeColor: Colors.white,
-            );
-          },
-        ),
-        belowBarData: BarAreaData(show: false),
-      ),
-    ];
-  }
-
-  LineTouchData buildTouchData() {
-    return LineTouchData(
-      touchTooltipData: LineTouchTooltipData(
-        maxContentWidth: 150,
-        getTooltipColor: (touchedSpot) => Color.fromARGB(220, 26, 23, 74),
-        tooltipBorderRadius: BorderRadius.circular(8.0),
-        getTooltipItems: (List<LineBarSpot> touchedBarSpots) {
-          return touchedBarSpots.map((barSpot) {
-            final index = barSpot.x.toInt();
-            if (index >= 0 && index < dataPoints!.length) {
-              return LineTooltipItem(
-                "วันที่: ${dataPoints![index].fullLabel}\n ค่า: ${dataPoints![index].value.toStringAsFixed(3)}\n เตา: ${dataPoints![index].furnaceNo}\n เลขแมต: ${dataPoints![index].matNo} ",
-                AppTypography.textBody3W,
-                textAlign: TextAlign.left
-              );
-            }
-            return null;
-          }).whereType<LineTooltipItem>().toList();
-        },
-      ),
-      handleBuiltInTouches: true,
-    );
-  }
-
-  Widget buildLegend() {
-    return Wrap(
-      spacing: 16,
-      runSpacing: 8,
-      direction: Axis.vertical,
-      children: [
-        buildLegendItem('USL', Colors.red, false, formatValue(controlChartStats?.specAttribute?.surfaceHardnessUpperSpec)),
-        buildLegendItem('UCL', Colors.orange, false, formatValue(controlChartStats?.controlLimitIChart?.ucl)),
-        buildLegendItem('AVG', Colors.green, false, formatValue(controlChartStats?.average)),
-        buildLegendItem('LCL', Colors.orange, false, formatValue(controlChartStats?.controlLimitIChart?.lcl)),
-        buildLegendItem('LSL', Colors.red, false, formatValue(controlChartStats?.specAttribute?.surfaceHardnessLowerSpec)),
-      ],
-    );
-  }
-
-  Widget buildLegendItem(String label, Color color, bool isDashed, String? value) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        SizedBox(
-          width: 8,
-          height: 2,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: color,
-              border: isDashed ? Border.all(color: color, width: 1) : null,
-            ),
-            child: isDashed
-                ? CustomPaint(
-                    painter: DashedLinePainter(color: color),
-                  )
-                : null,
-          ),
-        ),
-        const SizedBox(width: 8),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              label,
-              style: const TextStyle(
-                fontSize: 10,
-                color: AppColors.colorBlack,
-              ),
-            ),
-            Text(
-              value ?? 'N/A',
-              style: const TextStyle(
-                fontSize: 10,
-                color: AppColors.colorBlack,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-
-  double _getInterval() {
-    final spotMin = getMinSpot();
-    final spotMax = getMaxSpot();
-    final range = (spotMax - spotMin).abs();
-    
-    if (range < 10) {
-      return 2.5; // hardcode สำหรับ range เล็ก
+    if (res.statusCode != null && res.statusCode! >= 200 && res.statusCode! < 300) {
+      return res.data as T;
     }
 
-    if (range < 5) {
-      return 1.25; // hardcode สำหรับ range เล็ก
-    }
-    
-    final targetIntervals = 2;
-    final tempInterval = range / targetIntervals;
-    
-    // Conditional interval selection
-    if (tempInterval < 25) return 25.0;
-    else if (tempInterval < 50) return 50.0;
-    else if (tempInterval < 75) return 75.0;
-    else if (tempInterval < 100) return 100.0;
-    else return (tempInterval / 100).ceil() * 100.0; // สำหรับค่าใหญ่กว่า
+    throw DioException(
+      requestOptions: res.requestOptions,
+      response: res,
+      type: DioExceptionType.badResponse,
+      error: 'HTTP ${res.statusCode}: ${res.data}',
+    );
   }
 
-  double getMinY() {
-    final controlLCL = controlChartStats?.controlLimitIChart?.lcl;
-    final specLower = controlChartStats?.specAttribute?.surfaceHardnessLowerSpec;
-    final spotMin = getMinSpot();
-    final spotMax = getMaxSpot();
-    final range = (spotMax - spotMin).abs();
-    
-    // คำนวณ base min
-    double baseMin = spotMin;
-    if (specLower != null && specLower > 0) {
-      baseMin = min(baseMin, specLower * 0.95);
-    } else if (controlLCL != null && controlLCL > 0) {
-      baseMin = min(baseMin, controlLCL * 0.95);
-    }
-    
-    final interval = _getInterval();
-    final calculatedMin = (baseMin / interval).floor() * interval;
-    return max(0.0, calculatedMin);
+  Future<T> put<T>(String endpoint, dynamic data) async {
+    final response = await _dio.put(endpoint, data: data);
+    return response.data as T;
   }
 
-  double getMaxY() {
-    final controlUCL = controlChartStats?.controlLimitIChart?.ucl;
-    final specUpper = controlChartStats?.specAttribute?.surfaceHardnessUpperSpec;
-    final spotMin = getMinSpot();
-    final spotMax = getMaxSpot();
-    (spotMax - spotMin).abs();
-    
-    // คำนวณ base max
-    double baseMax = spotMax;
-    if (specUpper != null && specUpper > 0) {
-      baseMax = max(baseMax, specUpper * 1.05);
-    } else if (controlUCL != null && controlUCL > 0) {
-      baseMax = max(baseMax, controlUCL * 1.05);
-    }
-    
-    final interval = _getInterval();
-    return (baseMax / interval).ceil() * interval;
+  Future<T> delete<T>(String endpoint) async {
+    final response = await _dio.delete(endpoint);
+    return response.data as T;
   }
-
-  double _calculateYAxisInterval() {
-    return _getInterval(); // ใช้ interval ที่คำนวณแล้ว
-  }
-    
-    double _calculateXInterval() {
-      int pointCount = dataPoints!.length;
-      
-      if (pointCount <= 10) return 1.0;
-      return (pointCount / 10).ceilToDouble();
-    }
-
-  double getMaxSpot() {  
-  final maxSpot = dataPoints!
-      .map((point) => point.value)
-      .where((value) => value > 0)
-      .fold<double>(double.negativeInfinity, max);
-
-  return maxSpot;
-  }
-
-  double getMinSpot() {
-  // if (dataPoints == null || dataPoints!.isEmpty || controlChartStats!.specAttribute!.surfaceHardnessLowerSpec == 0 || controlChartStats!.specAttribute!.surfaceHardnessLowerSpec == null) {
-  //   return 0.0;
-  // }
-  
-  final minSpot = dataPoints!
-      .map((point) => point.value)
-      .where((value) => value > 0)
-      .fold<double>(double.infinity, min);
-  
-  return minSpot;
-  }
-  String formatValue(double? value) {
-    if (value == null) return 'N/A';
-    return value.toStringAsFixed(3);
-  }
-}
+} 
